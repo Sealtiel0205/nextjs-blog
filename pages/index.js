d68fe7ed731cb3883160792b13f1d9bf21dc602b
Sealtiel Salazar <sealtielnielsenm.salazar@gmail.com>
2026-08-13 17:59:36 +0800

    Update website

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello there! the name is Sealtiel Nielsen M. Salazar, and welcome to my page!]</p>
+        <p>[Hello! the name is Sealtiel Nielsen M. Salazar, and excited to learn about Next.js and web development]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
